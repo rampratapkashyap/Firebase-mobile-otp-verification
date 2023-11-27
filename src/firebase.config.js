6d3169a507e9f7de8f9581810202c61bc35ae5ae
ramkashyap <rampratap.kashyap@razorse.com-at-099391646783>
2023-11-27 18:19:27 +0530

    API key
    API key

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB3R8lLSa-NKgI-bt5Xg-4UEVC3Xiez42I",
-  authDomain: "exel-world.firebaseapp.com",
-  projectId: "exel-world",
-  storageBucket: "exel-world.appspot.com",
-  messagingSenderId: "295598557151",
-  appId: "1:295598557151:web:7ccb4ee5b619d7762ea66a",
-  measurementId: "G-331CT12BRG"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
